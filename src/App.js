7431a917fb2a10ebf17166e96a9bf6cbc76e777a
StoneDog02 <87345253+StoneDog02@users.noreply.github.com>
2021-11-02 18:20:11 -0600

    centered game container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ const GameContainer = styled.div`
   height: 330px;
   width: 330px;
   background: rebeccapurple;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const Timer = styled.div`
@@ -12,6 +16,8 @@ const Timer = styled.div`
   width: 50px;
   background: black;
   color: red;
+  position: absolute;
+  left: 280px;
 `;
 
 export default function App() {
