1ce5e7500dc5bf73a8d9addd7043713a710443cf
StoneDog02 <87345253+StoneDog02@users.noreply.github.com>
2021-11-02 18:25:45 -0600

    added button reset

--- src/App.js
+++ src/App.js
@@ -18,12 +18,42 @@ const Timer = styled.div`
   color: red;
   position: absolute;
   left: 280px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FlagCounter = styled.div`
+  height: 30px;
+  width: 50px;
+  background: black;
+  color: red;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Smiley = styled.button`
+  height: 30px;
+  width: 30px;
+  background: black;
+  color: red;
+  position: absolute;
+  display: flex;
+  left: 50%;
+  justify-content: center;
+  align-items: center;
+  transform: translate(-50%);
+  cursor: pointer;
 `;
 
 export default function App() {
   return (
     <GameContainer>
       <Timer>25</Timer>
+      <FlagCounter>10</FlagCounter>
+      <Smiley>😎</Smiley>
     </GameContainer>
   );
 }
